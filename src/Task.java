5ab890899b019efa879632204cbbbff4a01ef55e
lnxz <lnx@live.com.sg>
2016-02-23 15:41:21 +0800

    Revert "Added LoadTask method"
    This reverts commit 868f644372c165e3537d539acf1bf50187028a6e.

--- src/Task.java
+++ src/Task.java
@@ -13,51 +13,27 @@ public class Task {
 	private taskState _state;
 	private Date _dateAdded;
 	
-	// Overload Constructor 1 - New Floating Task 
+	// Overload Constructor 1 - Floating Task 
 	public Task(String name){
 		this.set_name(name);
 		this.set_dateAdded(new Date()); // Current date time
 		this.set_state(taskState.OPEN);
 	}
 	
-	// Overload Constructor 2 - New Deadline Task 
+	// Overload Constructor 2 - Deadline Task 
 	public Task(String name, Date endDate){
 		this.set_name(name);
-		this.set_endDate(endDate);
+		this._endDate = endDate;
 		this.set_dateAdded(new Date()); // Current date time
 		this.set_state(taskState.OPEN);
 	}
 	
-	// Overload Constructor 3 - New Event Task 
+	// Overload Constructor 3 - Event Task 
 	public Task(String name, Date startDate, Date endDate){
 		this.set_name(name);
-		this.set_startDate(startDate);
-		this.set_endDate(endDate);
+		this._startDate = startDate;
+		this._endDate = endDate;
 		this.set_dateAdded(new Date()); // Current date time
-		this.set_state(taskState.OPEN);
-	}
-	
-	// Overload Constructor 4 - Import Floating Task 
-	public Task(String name, Date dateAdded, String state){
-		this.set_name(name);
-		this.set_dateAdded(dateAdded);
-		this.set_state(_state);
-	}
-	
-	// Overload Constructor 5 - Import Deadline Task 
-	public Task(String name, Date endDate, Date dateAdded, String state){
-		this.set_name(name);
-		this.set_endDate(endDate);
-		this.set_dateAdded(dateAdded);
-		this.set_state(_state);
-	}
-	
-	// Overload Constructor 6 - Import Event Task 
-	public Task(String name, Date startDate, Date endDate, Date dateAdded, String state){
-		this.set_name(name);
-		this.set_startDate(startDate);
-		this.set_endDate(endDate);
-		this.set_dateAdded(new Date());
 		this.set_state(taskState.OPEN);
 	}
 	
@@ -68,16 +44,16 @@ public class Task {
 		ID = iD;
 	}
 	private Date getStartDate() {
-		return get_startDate();
+		return _startDate;
 	}
 	private void setStartDate(Date startDate) {
-		this.set_startDate(startDate);
+		this._startDate = startDate;
 	}
 	private Date getEndDate() {
-		return get_endDate();
+		return _endDate;
 	}
 	private void setEndDate(Date endDate) {
-		this.set_endDate(endDate);
+		this._endDate = endDate;
 	}
 
 	private String get_name() {
@@ -102,22 +78,6 @@ public class Task {
 
 	private void set_dateAdded(Date _dateAdded) {
 		this._dateAdded = _dateAdded;
-	}
-
-	private Date get_endDate() {
-		return _endDate;
-	}
-
-	private void set_endDate(Date _endDate) {
-		this._endDate = _endDate;
-	}
-
-	private Date get_startDate() {
-		return _startDate;
-	}
-
-	private void set_startDate(Date _startDate) {
-		this._startDate = _startDate;
 	}
 
 	
