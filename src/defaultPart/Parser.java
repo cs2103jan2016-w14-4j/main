0848982be3e0d486adef84ef0eb4ccce0d2123c9
Julian Chan <frozzenhart@hotmail.com>
2016-03-06 23:30:49 +0800

    Started Integration of Logic into Parser
    Both logic and parser temporarily unable to be used, as I removed the
    parser constructor to start intergrating the logic component into
    parser.

--- src/defaultPart/Parser.java
+++ src/defaultPart/Parser.java
@@ -1,10 +1,23 @@
 package defaultPart;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Parser {
+	
+	private static final String FILE_NAME = "WURI.txt";
+
+	private static final String MESSAGE_TASK_ADDED = "added to %1$s: \"%2$s\"";
+	private static final String MESSAGE_TASK_EDITED = "Edited task %1$s";
+	private static final String MESSAGE_TASK_COMPLETED = "Marked task %1$s as complete";
+	private static final String MESSAGE_TASK_DELETED = "deleted task %1$s";
+	private static final String MESSAGE_SEARCH_NO_RESULT = "Did not find any phrase with the keywords %1$s";
+	private static final String MESSAGE_TASK_FOUND = "Found %1$s tasks";
+
+	private static final String MESSAGE_INVALID_INDEX = "Invalid index %1$s";
+	private static final String MESSAGE_INVALID_ARGUMENTS = "Invalid arguments %1$s";
 	
 	private static final List<String> commandList = makeCommandList();
 	
@@ -27,8 +40,125 @@ public class Parser {
 	private String _commandTypeStr;
 	private String _arguments;
 	
-	public Parser(String input) {
-		setCommandTypeAndArguments(input);
+	public CommandDetails executeCommand(String userInput) {
+		setCommandTypeAndArguments(userInput);
+		CommandDetails commandDetails = new CommandDetails(this.getCommandTypeStr());
+		switch (commandDetails.getCommandType()) {
+			case ADD :
+				addTask(commandDetails);
+				break;
+
+			case EDIT :
+				editTask(commandDetails);
+				break;
+
+			case MARK_AS_COMPLETE :
+				markTaskAsComplete(commandDetails);
+				break;
+
+			case DELETE :
+				deleteTask(commandDetails);
+				break;
+
+			case FIND :
+				findTask(commandDetails);
+				break;
+
+			case UNDO :
+				// todo
+				break;
+
+			case STORE :
+				// todo
+				break;
+
+			case QUIT :
+				break;
+		}
+		return commandDetails;
+	}
+	
+	private void addTask(CommandDetails commandDetails) {
+		//todo
+		Task newTask = new Task(parser.getTaskDescription());
+		newTask.setStartDate(parser.getStartDate());
+		newTask.setEndDate(parser.getEndDate());
+		newTask.setRecur(parser.getRecur());
+
+		Task.addTask(newTask);
+
+		commandDetails.setFeedback(String.format(MESSAGE_TASK_ADDED, newTask.toString()));
+	}
+	
+
+	private void editTask(CommandDetails commandDetails) {
+		//todo
+		int taskIndex = parser.getTaskIndex();
+
+		Task task = Task.getTask(taskIndex);
+
+		String description = parser.getTaskDescription();
+		task.setDescription(description);
+
+		TaskDate startDate = parser.getStartDate();
+		task.setStartDate(startDate);
+
+		TaskDate endDate = parser.getEndDate();
+		task.setEndDate(endDate);
+
+		Recur recur = parser.getRecur();
+		task.setRecur(recur);
+
+		if (description == null && startDate == null && endDate == null & recur == null) {
+			commandDetails.setCommandType(CommandDetails.CommandType.EDIT_SHOW_TASK);
+		} else {
+			commandDetails.setFeedback(String.format(MESSAGE_TASK_EDITED, taskIndex));
+		}
+	}
+
+	private void markTaskAsComplete(CommandDetails commandDetails) {
+		//todo
+		int taskIndex = parser.getTaskIndex();
+		Task task = Task.getTask(taskIndex);
+		if (task != null) {
+			task.setCompleted(true);
+			commandDetails.setFeedback(String.format(MESSAGE_TASK_COMPLETED, taskIndex));
+		} else {
+			commandDetails.setCommandType(CommandDetails.CommandType.ERROR);
+			commandDetails.setFeedback(String.format(MESSAGE_INVALID_INDEX, taskIndex));
+		}
+	}
+
+	private void deleteTask(CommandDetails commandDetails) {
+		//todo
+		int taskIndex = parser.getTaskIndex();
+		Task task = Task.getTask(taskIndex);
+		Recur recur = task.getRecur();
+
+		if (recur == null || !recur.willRecur() || parser.isDeletingRecur()) {
+			Task.removeTask(taskIndex);
+			commandDetails.setFeedback(String.format(MESSAGE_TASK_DELETED, taskIndex));
+		} else {
+			task.setEndDate(recur.getNextRecur());
+			commandDetails.setFeedback(String.format(MESSAGE_TASK_DELETED, taskIndex));
+		}
+	}
+
+	private void findTask(CommandDetails commandDetails) {
+		//todo
+		List<Integer> indexesFound = new ArrayList<Integer>();
+		String keywords = parser.getTaskDescription();
+		for (int i = 0; i < Task.getTaskCount(); i++) {
+			if (Task.getTask(i).getDescription().contains(keywords)) {
+				indexesFound.add(i);
+			}
+		}
+		commandDetails.setIndexesFound(indexesFound);
+		if (indexesFound.size() == 0) {
+			commandDetails.setFeedback(String.format(MESSAGE_SEARCH_NO_RESULT, keywords));
+		} else {
+			commandDetails.setFeedback(String.format(MESSAGE_TASK_FOUND, indexesFound.size()));
+		}
 	}
 	
 	private void setCommandTypeAndArguments(String input) {
