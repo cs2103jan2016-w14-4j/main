f014b0b41b74c98d8d27a375715f4962046a6e78
lnxz <lnx@live.com.sg>
2016-03-16 21:44:49 +0800

    Added Settings/Config

---
+++ src/defaultPart/Settings.java
@@ -0,0 +1,35 @@
+package defaultPart;
+
+import java.io.File;
+
+public class Settings {
+	private static String filepath;
+	private final static String defaultPath = "tasklist.xml";
+	private final static String settingsPath = "config.xml";
+	private Settings settingsObject;
+
+	private Settings() {
+		File configFile = new File(settingsPath);
+		if (configFile.isFile() && configFile.canRead()) {
+			// TODO get settings path from config file 
+		}else
+		{
+			filepath = defaultPath;
+		}
+	}
+	
+	public Settings getInstance() {
+		if (settingsObject == null) {
+			settingsObject = new Settings();
+		}
+		return settingsObject;
+	}
+
+	public static String getFilepath() {
+		return filepath;
+	}
+
+	public static void setFilepath(String filepath) {
+		Settings.filepath = filepath;
+	}
+}
