72b7f75696d90db7f4b17e3da974ef680df914d5
houruomu <houruomu@gmail.com>
2016-03-16 10:42:27 +0800

    Change the default title of the UI

--- src/tableUi/Main.java
+++ src/tableUi/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("layout1.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("WURI");
         Scene scene = new Scene(root, 800, 450);
        //  scene.getStylesheets().add(getClass().getResource("layout1.css").toExternalForm());
         primaryStage.setScene(scene);
