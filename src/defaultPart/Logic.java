1464b551504cdf7ebeeeb076a543b2713c0de570
riwu <riwu0730@gmail.com>
2016-03-23 20:17:00 +0800

    refactor

--- src/defaultPart/Logic.java
+++ src/defaultPart/Logic.java
@@ -1,8 +1,5 @@
 package defaultPart;
 
-import tableUi.Controller;
-
-import java.io.File;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -20,15 +17,11 @@ import java.util.logging.SimpleFormatter;
 import org.xml.sax.SAXException;
 
 public class Logic {
-<<<<<<< HEAD
-	//some comments
-=======
-	//another comments
->>>>>>> abda56f39102ae34dee6715a61f1399a2106baf0
+
 	private static final Logger logger = Logger.getLogger(Logic.class.getName());
 
 	private static final int LIST_NUMBERING_OFFSET = 1;
-	//some other places
+
 	private static final String MESSAGE_TASK_ADDED = "Added %1$s";
 	private static final String MESSAGE_TASK_EDITED = "Edited task %1$s";
 	private static final String MESSAGE_TASK_COMPLETED = "Marked task %1$s as %2$scomplete";
@@ -69,7 +62,7 @@ public class Logic {
 		setupLogger();
 		_storage = new Storage();
 	}
-	
+
 	public void loadTasksFromFile() throws SAXException {
 		Settings settings = Settings.getInstance();
 		_storage.loadTasksFromFile(settings.getTaskFilePathAndName());
