1deec4eb4994b61ebbfdc8535abba05426db6ed8
riwu <riwu0730@gmail.com>
2016-03-23 14:31:56 +0800

    comment

--- src/defaultPart/Logic.java
+++ src/defaultPart/Logic.java
@@ -20,7 +20,7 @@ import java.util.logging.SimpleFormatter;
 import org.xml.sax.SAXException;
 
 public class Logic {
-
+	//some comments
 	private static final Logger logger = Logger.getLogger(Logic.class.getName());
 
 	private static final int LIST_NUMBERING_OFFSET = 1;
