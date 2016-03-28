fb6ba3262c1a2c523b152ea5c28248584d473538
houruomu <houruomu@gmail.com>
2016-03-28 18:45:57 +0800

    Fix the bugs with help
    Press F1 to init help, and use either F1 or ESC to return

--- src/tableUi/HelpWindow.java
+++ src/tableUi/HelpWindow.java
@@ -3,14 +3,17 @@ package tableUi;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 import java.net.URL;
@@ -19,12 +22,12 @@ import java.util.ResourceBundle;
 /**
  * Created by houruomu on 2016/3/28.
  */
-public class HelpWindow implements Initializable {
+public class HelpWindow{
     private static HelpWindow ourInstance = new HelpWindow();
     private Stage window;
 
     @FXML
-    public AnchorPane pane;
+    public VBox root;
     public ImageView image;
 
     public static HelpWindow getInstance() {
@@ -34,34 +37,34 @@ public class HelpWindow implements Initializable {
     private HelpWindow() {
         window = new Stage();
         window.initModality(Modality.APPLICATION_MODAL);
-        window.setTitle("Help ESC to exit...");
+        window.initStyle(StageStyle.UTILITY);
+        window.setTitle("Help Window | Press ESC to exit...");
 
         try {
-            FXMLLoader loader = new FXMLLoader();
-            Scene scene = loader.load(getClass().getResource("help.fxml"));
-        } catch (IOException e) {
+            image = new ImageView(getClass().getResource("help.jpg").toExternalForm());
+            root = new VBox();
+            root.getChildren().add(image);
+            Scene scene = new Scene(root, 400, 400);
+            window.setScene(scene);
+            
+            scene.addEventHandler(KeyEvent.KEY_PRESSED, e->{
+                if(e.getCode().equals(KeyCode.ESCAPE) || e.getCode().equals(KeyCode.F1)){
+                    window.close();
+                }else{
+                    System.out.println("Key pressed!");
+                }
+            });
+        } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        pane.addEventHandler(KeyEvent.KEY_PRESSED, e->{
-            if(e.getCode().equals(KeyCode.ESCAPE)){
-                window.close();
-            }else{
-                System.out.println("Key pressed!");
-            }
-        });
-
-        image.setImage(new Image(getClass().getResource("hello.jpg").toExternalForm()));
     }
 
     public static void show(){
         getInstance().showWindow();
     }
 
-    private void showWindow(){
+    public void showWindow(){
         window.showAndWait();
+        root.requestFocus();
     }
 }
