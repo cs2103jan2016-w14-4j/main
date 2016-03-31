49eef49cad1c887acbd2d80dac1ac065affddd52
lnxz <lnx@live.com.sg>
2016-03-31 23:45:10 +0800

    Commented out the deletion of previous task list

--- src/defaultPart/Settings.java
+++ src/defaultPart/Settings.java
@@ -125,7 +125,7 @@ public class Settings {
 		if (savePath.charAt(savePath.length() - 1) != '/') {
 			_savePath = savePath + "/";
 		}
-		if (savePath.equals("/")) {
+		if (savePath.equals("/") || savePath.equals("\"")) {
 			_savePath = TASK_FILE_DEFAULT_PATH;
 		} else if (isValidPath(savePath)) {
 			_savePath = savePath;
