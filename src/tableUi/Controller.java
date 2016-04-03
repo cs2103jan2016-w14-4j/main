1976c3f6c649ec6fdc4b87c6f468a3ecce6c794e
houruomu <houruomu@gmail.com>
2016-04-03 19:40:29 +0800

    make the listener delay longer and add it back
    I have tested this code by exporting it out and run it. It shows no
    delay, please comment it out again if you see any delay on your
    computer.

--- src/tableUi/Controller.java
+++ src/tableUi/Controller.java
@@ -154,10 +154,10 @@ public class Controller implements Initializable {
 		// delayed setter (set only after the loading of the software is done)
 		new Thread(() -> {
 			try {
-				// Thread.sleep(100);
-				// root.widthProperty().addListener(e -> {
-				// resizeColumns();
-				// });
+				 Thread.sleep(1000);
+				 root.widthProperty().addListener(e -> {
+				 resizeColumns();
+				 });
 			} catch (Exception e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
