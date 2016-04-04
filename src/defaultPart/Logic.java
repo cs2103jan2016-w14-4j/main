c6d4551ed4f80e35140f6a104dd9b0e66f2c641f
Julian Chan <frozzenhart@hotmail.com>
2016-04-04 23:49:53 +0800

    Completed Fix, previous commit had bug

--- src/defaultPart/Logic.java
+++ src/defaultPart/Logic.java
@@ -344,6 +344,7 @@ public class Logic {
 				if (Character.isDigit(frequency)) {
 					recur.setFrequency(Character.getNumericValue(frequency));
 				}
+				task.setRecur(recur);
 				args.remove(frequencyAndUnit);
 				return true;
 			}
