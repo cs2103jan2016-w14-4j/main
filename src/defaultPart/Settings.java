497e6a2b3a3a7b1e348373e6526d5eed7c518464
lnxz <lnx@live.com.sg>
2016-04-11 22:50:08 +0800

    Changes for collate tool

--- src/defaultPart/Settings.java
+++ src/defaultPart/Settings.java
@@ -23,7 +23,7 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
 
-//@@author Shaun Lee
+//@@author A0125497W
 public class Settings {
 	/* File names & default paths */
 	private static final String TASK_FILE_NAME = "tasklist.xml";
--- src/test/StorageTest.java
+++ src/test/StorageTest.java
@@ -23,7 +23,7 @@ import defaultPart.Storage;
 import defaultPart.Task;
 import tableUi.Controller;
 
-//@@author Shaun Lee
+//@@author A0125497W
 public class StorageTest {
 
 	private static final Logger logger = Logger.getLogger(Controller.class.getName());
